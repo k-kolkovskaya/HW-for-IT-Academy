613120fc1764099d0a7467466f93ac6c0d4df0a7
Businka2611 <moszechok@gmail.com>
2019-06-27 21:59:15 +0300

    Return hw 13.1

---
+++ 13.1/src/modules/stopwatch.js
@@ -0,0 +1,15 @@
+import {StopwatchTimer} from './stopwatchTimer.js';
+
+function Stopwatch() {
+    StopwatchTimer.call(this, 'stopwatch', 0);
+}
+
+Stopwatch.prototype = Object.create(StopwatchTimer.prototype);
+
+Stopwatch.prototype.showInfo = function() {
+    console.log(this);
+}
+
+
+
+export { Stopwatch };
---
+++ 13.1/src/modules/timer.js
@@ -0,0 +1,14 @@
+import {StopwatchTimer} from './stopwatchTimer.js';
+
+function Timer() {
+    StopwatchTimer.call(this, 'timer', 300);
+}
+
+Timer.prototype = Object.create(StopwatchTimer.prototype);
+
+Timer.prototype.showInfo = function() {
+    console.log(this);
+}
+
+
+export { Timer };
