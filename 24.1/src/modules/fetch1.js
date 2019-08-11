766edb246c15e719caa5c3be8d8daa34a507b7a8
Businka2611 <moszechok@gmail.com>
2019-08-11 17:00:02 +0300

    Update 24.1

---
+++ 24.1/src/modules/fetch1.js
@@ -0,0 +1,17 @@
+import { Logger } from "./logger.js";
+
+class Fetch1 {
+  constructor(name) {
+    this.name = name;
+  }
+  
+  run(url) {
+    const before = new Date().getTime();
+    return fetch(url).then(() => {
+      const after = new Date().getTime();
+      return new Logger(this.name, before, after);
+    });
+  };
+}
+
+export { Fetch1 };
---
+++ 24.1/syncDelay1.js
@@ -0,0 +1,27 @@
+    
+import { Logger } from './logger.js';
+
+class SyncDelay1{
+    constructor(name){
+        this.name = name;
+    };
+
+    run = () => {
+     if(SyncDelay1.seconds === undefined){
+         throw new Error('SyncDelay1.seconds is not defined');
+     };
+
+     const seconds = SyncDelay1.seconds
+     const before = new Date().getTime();
+
+     for (let j = 0; j < seconds; j++){
+        for (let i = 0; i < 700000000; i++){
+            let a = 321;
+        };
+     };
+     const after = new Date().getTime();
+     return new Logger (this.name, before, after);
+    };
+};
+
+export { SyncDelay1 };
