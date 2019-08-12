53cae281ff06995b0781b52b7f8b4222ea18ed63
Businka2611 <moszechok@gmail.com>
2019-08-12 19:18:15 +0300

    Update 24.2

--- 24.2/src/modules/main.js
+++ 24.2/src/modules/main.js
@@ -3,33 +3,33 @@ class URLParser {
     this.url = url;
   }
 
-  RegExp = () =>{
-    const regExp = new RegExp('[?&]+([^=&]+)=([^&]*)', 'g');
-    const result = {};
-    this.url.replace(regExp, (key, value) => {
-      result[key] = value;
-    });
-  return result;
-  };
-
-  Split = () => {
+  getSplit() {
     const parts = this.url.split('?');
     const result = {};
-    if(parts.length === 2) {
-      const partRight = flinders[1];
-      const pairs = partRight.split('&');
-      pairs.forEach(cour => {
-        const detailsCouple = cour.split('=');
-        const name = detailsCouple[0];
-        const value = detailsCouple[1];
-        result[name] = value;
-      });
-      return result;
-    }
+    const secondPart = parts[1];
+    const newParts = secondPart.split('&');
+    
+    newParts.forEach(part => {
+      const meanings = part.split('=');
+      const name = meanings[0];
+      const definition = meanings[1];
+      result[name] = definition;
+    });
+
+    console.log(result);
   }
+
+  getRegExp() {
+    const regExp = new RegExp('[?&]+([^=&]+)=([^&]*)', 'g');
+    const result = {};
+    this.url.replace(regExp, (match, key, value) => {
+      result[key] = value;
+    });
+  console.log(result);
+  };
 }
 
-
 const urlParser = new URLParser('http://127.0.0.1:5500?a=1&b=&c=3');
-console.log(urlParser.RegExp());
-console.log(urlParser.Split());
+
+urlParser.getSplit();
+urlParser.getRegExp();
