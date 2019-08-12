0f100b1dacece20010cd63acea2e992a5c4b5c7f
Businka2611 <moszechok@gmail.com>
2019-08-12 18:38:20 +0300

    Update 25.2

--- 24.2/src/modules/main.js
+++ 24.2/src/modules/main.js
@@ -13,12 +13,12 @@ class URLParser {
   };
 
   Split = () => {
-    const flinders = this.url.split('?');
+    const parts = this.url.split('?');
     const result = {};
-    if(flinders.length === 2) {
+    if(parts.length === 2) {
       const partRight = flinders[1];
-      const cours = partRight.split('&');
-      cours.forEach(cour => {
+      const pairs = partRight.split('&');
+      pairs.forEach(cour => {
         const detailsCouple = cour.split('=');
         const name = detailsCouple[0];
         const value = detailsCouple[1];
@@ -26,8 +26,8 @@ class URLParser {
       });
       return result;
     }
-  };
-};
+  }
+}
 
 
 const urlParser = new URLParser('http://127.0.0.1:5500?a=1&b=&c=3');
--- 25.2/src/modules/main.js
+++ 25.2/src/modules/main.js
@@ -1,68 +1,108 @@
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+console.log(canvas);
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
 const mouse = {
   x: undefined,
   y: undefined
-};
+}
 
-addEventListener("mousemove", event => {
-  mouse.x = event.clientX;
-  mouse.y = event.clientY;
-});
+addEventListener('mousemove', function(e) {
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+})
 
-addEventListener("resize", () => {
+addEventListener('resize', function() {
   canvas.width = innerWidth;
   canvas.height = innerHeight;
-  init();
-});
+  initCircles();
+  initRectangles();
+})
 
-class Circle {
-  constructor(x, y, radius, color) {
-    this.y = y;
-    this.x = x;
-    this.color = color;
-    this.radius = radius;
-    this.velocity = {
-      x: Math.random() - 0.2,
-      y: Math.random() - 0.2
-    };
-  };
+function Circle(x, y, radius, color) {
+  this.x = x;
+  this.y = y;
+  this.radius = radius;
+  this.color = color;
+  this.velocity = {
+    x: Math.random(0.3),
+    y: Math.random(0.3)
+  }
+}
 
-  draw() {
-    ctx.fillStyle = this.color;
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    ctx.fill();
-    ctx.closePath();
-  };
-};
+//c.arc(x, y, radius, startAngle, endAngle, drawClockwise);
+Circle.prototype.draw = function() {
+  c.beginPath();
+  c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+  c.fillStyle = this.color;
+  c.fill();
+  c.closePath();
+}
 
-const update = function() {
+function Rectangle(x, y, width, height, color) {
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+  this.color = color;
+  this.velocity = {
+    x: Math.random(0.3),
+    y: Math.random(0.3)
+  }
+}
+
+//c.fillRect(x, y, width, height)
+Rectangle.prototype.draw = function() {
+  c.beginPath();
+  c.fillRect(this.x, this.y, this.width, this.height);
+  c.fillStyle = this.color;
+  c.fill();
+  c.closePath();
+}
+
+function update() {
   this.draw();
   this.x += this.velocity.x;
-  this.y += this.velocity.x;
-};
+  this.y += this.velocity.y;
+}
 
 let circles = [];
-const init = () => {
-  for (let i = 0; i < 800; i++) {
+function initCircles() {
+  for (let i = 0; i < 900; i++) {
     const x = Math.random() * canvas.width;
     const y = Math.random() * canvas.height;
     const radius = Math.random() * 5;
-    const color = "LightSlateGray";
+    const color = 'black';
     circles.push(new Circle(x, y, radius, color));
   }
-};
+}
 
-const animate = function() {
-  requestAnimationFrame(animate);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  circles.forEach(element => {
-    update.call(element);
+let rectangles = [];
+function initRectangles() {
+  for (let i = 0; i < 900; i++) {
+    const x = Math.random() * canvas.width;
+    const y = Math.random() * canvas.height;
+    const width = 8;
+    const height = 8;
+    const color = 'red';
+    rectangles.push(new Rectangle(x, y, width, height, color));
+  }
+}
+
+function animate() {
+  requestAnimationFrame(animate); 
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  circles.forEach(circle => {
+    update.call(circle);
   });
-};
+  rectangles.forEach(rectangle => {
+    update.call(rectangle);
+  })
+}
 
-init();
+initCircles();
+initRectangles();
 animate();
